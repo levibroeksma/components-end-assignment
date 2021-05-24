efb07d4f0585da35507072fd25034fe2bbc2c8a5
School <School@Levis-oude-bakbeest.local>
2021-05-25 00:25:21 +0200

    component builds for end assignments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import mexico from './assets/mexico.png';
+import kcal from './assets/meal.png';
+import clock from './assets/clock.png';
+import Recipe from "./Recipe";
+import logo from './logo.svg';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header
+        logo={logo}
+       />
+        {/*<Recipe*/}
+        {/*    title="Chipotle Chicken Taco's with blue corn tortilla's"*/}
+        {/*    description="These delicious mildly spicy, smokey chicken taco's are guaranteed to leave no leftovers!"*/}
+        {/*    imagecountry={mexico}*/}
+        {/*    preptimeimage={clock}*/}
+        {/*    kcalimage={kcal}*/}
+        {/*    preptime="45 min"*/}
+        {/*    kcal="475 kcal"*/}
+        {/*/>*/}
+
     </div>
-  );
+  )
 }
 
 export default App;
